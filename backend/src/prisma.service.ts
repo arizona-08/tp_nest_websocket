0395ab223e9a3b07d515bb6fa0d854d6985a53f8
Jonathan Assi <assijonathan2@gmail.com>
2026-03-05 01:32:14 +0100

    Passer de prisma-client a prisma-client-js

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "./generated/prisma/client";
-
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient{
+export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
     super({adapter})
